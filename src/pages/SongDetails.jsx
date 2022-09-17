16e6b290390668421020dda8170aac3554282790
Tidbits JS <tidbitsjs@gmail.com>
2022-09-17 19:02:23 +0530

    song lyrics

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -27,6 +27,8 @@ const SongDetails = () => {
     dispatch(playPause(true));
   };
 
+  console.log(songData);
+
   return (
     <div className="flex flex-col">
       <DetailsHeader
@@ -38,9 +40,13 @@ const SongDetails = () => {
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
 
         <div className="mt-5">
-          {songData?.sections[1]?.text.map((line, i) => (
-            <p key={`lyrics-${line}-${i}`} className="text-gray-400 text-base my-1">{line}</p>
-          ))}
+          {songData?.sections[1].type === 'LYRICS'
+            ? songData?.sections[1]?.text.map((line, i) => (
+              <p key={`lyrics-${line}-${i}`} className="text-gray-400 text-base my-1">{line}</p>
+            ))
+            : (
+              <p className="text-gray-400 text-base my-1">Sorry, No lyrics found!</p>
+            )}
         </div>
       </div>
 
